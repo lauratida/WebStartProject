e89685b230ef16c90271734f5cea8598874082dc
Nataly <html-tida@mail.ru>
2019-08-05 23:39:03 +0300

    Изменение модального окна(упрощение), скрипт открытия закрытия модального окна на JQuery, добавление простой кнопки наверх.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,19 @@
 var button = document.querySelector('#navbar-button');
-// console.log(button);
 var modal = document.querySelector('#modal1');
 var close = document.querySelector('#closemodal');
+let timerId = setTimeout(() => {
+  modal.classList.remove('modal_active');
+}, 1000);
 
 button.addEventListener('click', function(){
-  console.log(modal.classList);
   modal.classList.add('modal_active');
+  clearTimeout(timerId);
+  timerId = setTimeout(() => {
+    modal.classList.remove('modal_active');
+  }, 5000);
 });
 
 close.addEventListener('click', function(){
   modal.classList.remove('modal_active');
 });
-
 
---
+++ src/js/modal.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  var button = $('#navbar-button');
+  // console.log(button);
+  var modal = $('#modal1');
+  var close = $('#closemodal');
+
+  button.on('click', function(){
+    modal.addClass('modal_active');
+  });
+  close.on('click', function(){
+    modal.removeClass('modal_active');
+  });
+  // modal.on('click', function(){
+  //   modal.removeClass('modal_active');
+  // });
+  // $('#modal').click(function() {
+  //   if ($(event.target).closest('#modal').length == 0) {
+  //       $(this).removeClass('modal_active');
+  //   }                    
+  // });
+
+  $('#scrollbutton').on('click', function(){
+    // сперва получаем позицию элемента относительно документа
+    var scroll = $('#scroll').offset().top;
+    // скроллим страницу на значение равное позиции элемента
+    $("html, body").animate({ scrollTop: scroll }, 600);
+    return false;
+  });
+
+  // Проверка метода по скроллу к нужному месту.
+  // $('#herobutton').on('click', function(){
+  //   $("html, body").animate({ scrollTop: $('#hero').offset().top }, 600);
+  //   return false;
+  // });
+});
