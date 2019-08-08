ca5b94433d9faade624b8d8a6df63715c7d1f408
Nataly <html-tida@mail.ru>
2019-08-08 16:27:10 +0300

    Версия со слайдером клик

---
+++ src/js/form.js
@@ -0,0 +1,73 @@
+// Обработка и отправка формы через технологию Ajax
+
+$(document).ready(function(){
+  // Отправка формы из секции call
+  $('#call-form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      url: 'mail.php',
+      type: 'post',
+      data: $(this).serialize(),
+      success: function(data) {
+        // Вывод ответа в поля под кнопкой
+        // $('.call-desc__text1').html('<strong>' + data + '</strong>, наш менеджер перезвонит Вам&nbsp;в&nbsp;течение 60 секунд');
+        // $('.call-desc__text2').html('');
+        // Очистка полей формы
+        $('#username').val('');
+        $('#userphone').val('');
+        // Вывод ответа в модальном окне
+        $('#sucess-user').html(data);
+        $('#sucess-user').addClass('bold');
+        $('#modal-succes').addClass('modal_active');
+        // Автозакрытие
+        var timerId;
+        timerId = setTimeout(() => {
+          $('#modal-succes').removeClass('modal_active');
+        }, 5000);
+        // Закрытие на кнопку-крестик
+        $('#closesuccessmodal').on('click', function(){
+          clearTimeout(timerId);
+          $('#modal-succes').removeClass('modal_active');
+        });
+        
+      },
+      error: function(jqXHR, textStatus) {
+        console.log(jqXHR + ': ' + textStatus);
+      },
+    });
+  });
+
+  
+  // Отправка формы из модального окна
+  $('#dialog-form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      url: 'mail.php',
+      type: 'post',
+      data: $(this).serialize(),
+      success: function(data) {
+        // Очистка полей формы
+        $('#usernamed').val('');
+        $('#userphoned').val('');
+        // Вывод ответа в модальном окне
+        $('#sucess-user').html(data);
+        $('#sucess-user').addClass('bold');
+        $('#modal-succes').addClass('modal_active');
+        // Автозакрытие
+        var timerId;
+        timerId = setTimeout(() => {
+          $('#modal-succes').removeClass('modal_active');
+        }, 5000);
+        // Закрытие на кнопку-крестик
+        $('#closesuccessmodal').on('click', function(){
+          clearTimeout(timerId);
+          $('#modal-succes').removeClass('modal_active');
+        });
+        
+      },
+      error: function(jqXHR, textStatus) {
+        console.log(jqXHR + ': ' + textStatus);
+      },
+    });
+  });
+});
