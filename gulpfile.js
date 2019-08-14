e76302077b512e0fbf59292b3c3e00b388332548
Nataly <html-tida@mail.ru>
2019-08-14 20:59:42 +0300

    последняя версия перед редактированием подгрузки яндекскарты

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+var uglifyjs = require('uglify-js'); 
+var composer = require('gulp-uglify/composer');
+var pump = require('pump');
+var minify = composer(uglifyjs, console);
+var rename = require("gulp-rename");
+var tinypng = require('gulp-tinypng-compress');
+
+gulp.task('default', defaultTask)
+  function defaultTask(cb) {
+    cb();
+}
+
+gulp.task('minify-css', function(cb) {
+  return gulp.src('src/css/*.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(gulp.dest('dist/css/'))
+  cb();
+})
+
+gulp.task('minifyHtml', () => {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist/'));
+});
+
+ 
+gulp.task('minifyJs', function (cb) {
+  // the same options as described above
+  var options = {};
+  pump([
+      gulp.src(['src/js/*.js', '!src/js/*min.js']),
+      minify(options),
+      rename({suffix: '.min'}),
+      gulp.dest('dist/js')
+    ],
+    cb
+  );
+  gulp.src('src/js/*min.js')
+  .pipe(gulp.dest('dist/js/'))
+});
+
+gulp.task('fonts', function(cb) {
+  return gulp.src('src/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts/'))
+  cb();
+});
+
+gulp.task('tinypng', function (cb) {
+    gulp.src('src/img/**/*.{png,jpg,jpeg}')
+        .pipe(tinypng({
+            key: 'n4rqh4VKdVrXB6G0nyQlFnfH08zMclZS'
+        }))
+        .pipe(gulp.dest('dist/img'));
+    cb();
+});
+
+gulp.task('movephp', function(cb) {
+  return gulp.src('src/*.php')
+  .pipe(gulp.dest('dist/'))
+  cb();
+});
--- src/js/form.js
+++ src/js/form.js
@@ -57,7 +57,7 @@ $(document).ready(function(){
               $('#modal-succes').addClass('modal_active');
               // Автозакрытие
               var timerId;
-              timerId = setTimeout(() => {
+              timerId = setTimeout(function() {
                 $('#modal-succes').removeClass('modal_active');
               }, 5000);
               // Закрытие на кнопку-крестик
@@ -80,82 +80,3 @@ $(document).ready(function(){
   });
 
 });
-
-
-
-
-// Обработка и отправка формы через технологию Ajax
-
-// $(document).ready(function(){
-//   // Отправка формы из секции call
-//   $('#call-form').on('submit', function(event){
-//     event.preventDefault();
-//     $.ajax({
-//       url: 'mail.php',
-//       type: 'post',
-//       data: $(this).serialize(),
-//       success: function(data) {
-//         // Вывод ответа в поля под кнопкой
-//         // $('.call-desc__text1').html('<strong>' + data + '</strong>, наш менеджер перезвонит Вам&nbsp;в&nbsp;течение 60 секунд');
-//         // $('.call-desc__text2').html('');
-//         // Очистка полей формы
-//         $('#username').val('');
-//         $('#userphone').val('');
-//         // Вывод ответа в модальном окне
-//         $('#sucess-user').html(data);
-//         $('#sucess-user').addClass('bold');
-//         $('#modal-succes').addClass('modal_active');
-//         // Автозакрытие
-//         var timerId;
-//         timerId = setTimeout(() => {
-//           $('#modal-succes').removeClass('modal_active');
-//         }, 5000);
-//         // Закрытие на кнопку-крестик
-//         $('#closesuccessmodal').on('click', function(){
-//           clearTimeout(timerId);
-//           $('#modal-succes').removeClass('modal_active');
-//         });
-
-//       },
-//       error: function(jqXHR, textStatus) {
-//         console.log(jqXHR + ': ' + textStatus);
-//       },
-//     });
-//   });
-
-
-//   // Отправка формы из модального окна
-  // $('#dialog-form').on('submit', function(event){
-  //   event.preventDefault();
-  //   $.ajax({
-  //     url: '../mail.php',
-  //     type: 'post',
-  //     data: $(this).serialize(),
-  //     success: function(data) {
-  //       // Очистка полей формы
-  //       console.log('ghbdn');
-
-  //       $('#username').val('');
-  //       $('#userphone').val('');
-  //       // Вывод ответа в модальном окне
-  //       $('#sucess-user').html(data);
-  //       $('#sucess-user').addClass('bold');
-  //       $('#modal-succes').addClass('modal_active');
-  //       // Автозакрытие
-  //       var timerId;
-  //       timerId = setTimeout(() => {
-  //         $('#modal-succes').removeClass('modal_active');
-  //       }, 5000);
-  //       // Закрытие на кнопку-крестик
-  //       $('#closesuccessmodal').on('click', function(){
-  //         clearTimeout(timerId);
-  //         $('#modal-succes').removeClass('modal_active');
-  //       });
-
-  //     },
-  //     error: function(jqXHR, textStatus) {
-  //       console.log(jqXHR + ': ' + textStatus);
-  //     },
-  //   });
-  // });
-// });
