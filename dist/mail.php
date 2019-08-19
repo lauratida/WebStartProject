82964f8e89be47f9f32b090908e2c7cd721566f2
Nataly <html-tida@mail.ru>
2019-08-19 19:37:51 +0300

    Последняя версия до больницы

--- dist/mail.php
+++ dist/mail.php
@@ -1,44 +1,45 @@
-<?php
+<?php 
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 $data = $_POST;
-echo $data['username'];
-// echo $data['userphone'];
-// echo json_encode($data, JSON_UNESCAPED_UNICODE);
-// $name = $data['username'];
-// $phone = $data['userphone'];
-// // $email = $_POST['user_email'];
+$name = $data['username'];
+$phone = $data['userphone'];
+$email = $data['usermail'];
+$form = $data['formname'];
 
-// //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-// $mail->isSMTP();                                      // Set mailer to use SMTP
-// $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-// $mail->SMTPAuth = true;                               // Enable SMTP authentication
-// $mail->Username = 'mailsfromweb@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-// $mail->Password = 'ztpifburg12'; // Ваш пароль от почты с которой будут отправляться письма
-// $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-// $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'html-tida@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'clesopeb1'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-// $mail->setFrom('mailsfromweb@mail.ru'); // от кого будет уходить письмо?
-// $mail->addAddress('html-tida@mail.ru');     // Кому будет уходить письмо 
-// //$mail->addAddress('ellen@example.com');               // Name is optional
-// //$mail->addReplyTo('info@example.com', 'Information');
-// //$mail->addCC('cc@example.com');
-// //$mail->addBCC('bcc@example.com');
-// //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-// //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-// $mail->isHTML(true);                                  // Set email format to HTML
+$mail->setFrom('html-tida@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('html-tida@mail.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
 
-// $mail->Subject = 'Заявка с тестового сайта';
-// $mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ';
-// $mail->AltBody = '';
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = 'Заявка из формы: "'.$form . '".<br> Пользователь ' .$name . ' оставил заявку,<br>его(ее) телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
 
-// if(!$mail->send()) {
-//     echo 'Error';
-// } else {
-//     echo $data['username'];
-// }
+
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+  echo $data['username'];
+  
+}
 ?>
